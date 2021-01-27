027cecdf318ddc9a54b0acd6250b26c8f9bcd0a2
John Cokins <il7148pg@go.minnstate.edu>
2021-01-27 15:59:40 -0600

    welcome banner added'

--- camelcase_branching.py
+++ camelcase_branching.py
@@ -1,10 +1,17 @@
 def camelcase(sentence):
-
+    """convert sentence to camelCase"""
     title_case = sentence.title()
     upper_camel_cased = title_case.replace(' ', '')
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
+def banner():
+    """display welcome banner"""
+    message = 'CAMELCASE PROGRAM'
+    stars = '*' * len(message)
+    print(f'{stars}\n{message}\n{stars}')
+
 def main():
+    banner()
     sentence = input('Enter your sentence: ')
     output = camelcase(sentence)
     print(output)
